97e453aa464d1687b65053bd5a24e8917b38cf63
Findryankp <findryankurnia@gmail.com>
2017-11-13 06:34:28 +0700

    menjadikan semua menjadi fungsi(c.py)

--- c.py
+++ c.py
@@ -5,25 +5,53 @@ import getpass
 import MySQLdb
 from cryptography.fernet import Fernet
 
-#menambah satu akun
-def daftar():
+def conn(sql):
+	#localhost,root,password,nama data base
+	db = MySQLdb.connect("localhost", "root", "password","TESTDB")
+    cursor =db.cursor()
+    cek = cursor.execute(sql)
+    if cek :
+    	db.commit()
+    else :
+    	print "query gagal dieksekusi"
+    db.close()
+
+def pilihan():
+	print "1. Login\n"
+	print "2. Daftar\n"
+	pilihan = raw_input ("Masukan pilihan : ")
+
+	if pilihan == "1":
+		masuk()
+		server.connect((IP_address, Port))
+
+	else pilihan == "2":
+		daftar()
+
+#masuk akun
+def masuk():
+	print "Login Akun"
 	username = raw_input ("username akun : ")
 	password = getpass.getpass ("masukan password : ")
-	db = MySQLdb.connect("localhost", "root", "password","TESTDB")
-        cursor =db.cursor()
+	cekpwd(username,password)
 
-        sql ="INSERT INTO users (id,username,password,status,grup,private) VALUES ('null',username,password,0,0,0)"
-        cursor.execute(sql)
-        db.commit()
+#menambah satu akun
+def daftar():
+	print "Daftar Akun Baru"
+	username = raw_input ("username akun : ")
+	password = getpass.getpass ("masukan password : ")
+	
+    sql ="INSERT INTO users (id,username,password,status,grup,private) VALUES ('null','%s','%s',0,0,0)"%(username, password)
+    conn(sql)
+    masuk()
 
 #menemukan pengguna yang online
 def cekstatus():
 	db = MySQLdb.connect("localhost", "root", "password","TESTDB")
-        cursor =db.cursor()
-
-        #mencari status pada db yang bernilai satu
-        sql ="SELECT username FROM users WHERE status = 1"
-        cursor.execute(sql)
+    cursor =db.cursor()
+    #mencari status pada db yang bernilai satu
+    sql ="SELECT username FROM users WHERE status = 1"
+    cursor.execute(sql)
 	cek = cursor.fetchall()
 	for row in cek:
 		#mencetak semua yang online
@@ -31,19 +59,13 @@ def cekstatus():
 
  #merubah status menjadi offline
 def logoutstatus(username, password):
-	db = MySQLdb.connect("localhost", "root", "password","TESTDB")
-        cursor =db.cursor()
 	sql="UPDATE users SET status = 0 WHERE username = '%s' AND password = '%s'" %(username, password)
-        cursor.execute(sql)
-	db.commit()
+    conn(sql)
 
 #merubah status menjadi online
 def loginstatus(username, password):
-	db = MySQLdb.connect("localhost", "root", "password","TESTDB")
-        cursor =db.cursor()
-        sql="UPDATE users SET status = 1 WHERE username = '%s' AND password = '%s'" %(username, password)
-        cursor.execute(sql)
-	db.commit()
+    sql="UPDATE users SET status = 1 WHERE username = '%s' AND password = '%s'" %(username, password)
+    conn(sql)
 
 #cek ketersediaan akun
 def cekpwd(username, password):
@@ -60,27 +82,21 @@ def cekpwd(username, password):
 	else:
 		return 0
 
+
+#MAIN PROGRAM
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
 	print "Correct usage: script, IP address, port number"
 	exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
-print "1. Login\n"
-print "2. Daftar\n"
-pilihan = raw_input ("Masukan pilihan : ")
 
-if pilihan == "1":
-	username = raw_input ("username : ")
-	password = getpass.getpass("password : ")
-	server.connect((IP_address, Port))
-
-else pilihan == "2":
-	daftar();
+pilihan()
 
 a = cekpwd(username,password)
 if a==0:
 	print "gagal login"
+
 else:
 	loginstatus(username, password)
 	while True:
